33137b901168a1a3c1e8db0287cb209090bd54df
Zonkkatul <mervezonkk@gmail.com>
2024-12-02 01:05:42 +0100

    counter added

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,8 +1,25 @@
 import Tour from "./Components/Tour.jsx";
+import { BrowserRouter, Routes, Route} from "react-router-dom";
+import Home from "./Pages/Home.jsx";
+import About from "./Pages/About.jsx";
+import Checkout from "./Pages/Checkout.jsx";
+import NavBar from "./NavBar.jsx";
+import Footer from "./Footer.jsx";
+
 function App() {
-return <>
-<Tour />
-</>;
+    return (
+        <>
+        <BrowserRouter>
+        <NavBar></NavBar>
+        <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/about" element={<About/>}/>
+            <Route path="/checkout" element={<Checkout/>}/>
+        </Routes>
+        <Footer></Footer>
+        </BrowserRouter>
+        </>
+    )
 }
 
 export default App;
